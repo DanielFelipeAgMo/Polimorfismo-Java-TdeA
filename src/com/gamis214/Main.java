8b8824d1c9ab1e6635840125b6497bd8fd6f41f6
DanielFelipeAgMo <danielagudelo87@gmail.com>
2022-03-30 09:02:09 -0500

    Comentario inicial del proyecto

---
+++ src/com/gamis214/Main.java
@@ -0,0 +1,18 @@
+package com.gamis214;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //-->Declaracion del objeto PERRO
+	    Animal perro = new Perro("Stich","Carnivoro",15,"Doberman");
+        perro.Alimentarse();
+            //-->Declaracion del objeto Gato
+        Animal gato = new Gato("Pelusa","Galletas",15,"Siames");
+        gato.Alimentarse();
+        //-->Declaracion del objeto Caballo
+        Animal caballo = new Caballo("Spark","Pasto",25,"Pony");
+        caballo.Alimentarse();
+
+    }
+}
